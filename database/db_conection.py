ef38efcf07676999b514c8333a04ab2aefad089c
Samara <samarabsouz@gmail.com>
2024-10-25 22:56:10 -0300

    comentarios das atualizacoes

--- database/db_conection.py
+++ database/db_conection.py
@@ -1,12 +1,21 @@
-import streamlit as st
-import mysql.connector
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+DATABASE_URL = os.getenv("MYSQL_URL")
 
-def conectar_banco():
-    conn = mysql.connector.connect(
-        host="mysql.railway.internal", 
-        user="root",
-        password="rZfgEyBBGafsSzBCLFRJvXIUBIqoHpOw",
-        database="railway"
-    )
-    return conn
+# Configurar o engine de conexão com o banco de dados
+engine = create_engine(DATABASE_URL)
+
+# Configurar a sessão para interagir com o banco de dados
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Função para obter a sessão
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
